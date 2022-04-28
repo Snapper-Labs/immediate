21ec4e89e76a67ba9940ccd796ef5f3a4e3d223a
Ankit Kumar <ankit@apkumar.com>
2022-04-28 11:27:46 -0500

    basic

---
+++ go/web/examples/bk-deploy/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	_"log"
+	"time"
+
+	"github.com/apkumar/immediate/go"
+	"github.com/apkumar/immediate/go/web"
+)
+
+type app struct {
+}
+
+// nil while fetching.
+func useDeployableCommits(ui *immgo.Renderer) *[]string {
+	commits := immgo.State[*[]string](ui, nil)
+	if immgo.FirstRender(ui) {
+		go func() {
+			// pretend we're fetching commits from BK
+			time.Sleep(300 * time.Millisecond)
+
+			*commits = &[]string{"One", "Two", "Three"}
+		}()
+	}
+
+	return *commits
+}
+
+func commitDropdown(ui *immgo.Renderer) {
+	choices := useDeployableCommits(ui)
+	if choices == nil {
+		immgo_web.Text(ui, "Loading commits...")
+		return 
+	}
+
+	Dropdown(ui, *choices)
+}
+
+func (this *app) Render(ui *immgo.Renderer, doc *immgo_web.Document) {
+	if loaded := ShoelaceAssets(ui); !loaded {
+		immgo_web.Text(ui, "loading...")
+		return 
+	}
+
+	commitDropdown(ui)
+}
+
+func main() {
+	immgo_web.Serve("localhost:8080", &app{})
+}
---
+++ go/web/examples/bk-deploy/shoelace.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"github.com/apkumar/immediate/go"
+	"github.com/apkumar/immediate/go/web"
+)
+
+func WithSlot(slot string) immgo.RenderOption {
+	return immgo.WithAttribute("slot", slot)
+}
+
+// Simple dropdown, doesn't expose the entire shoelace api
+func Dropdown(ui *immgo.Renderer, choices []string, opts ...immgo.RenderOption) string {
+	choice := immgo.State(ui, "")
+
+	immgo.Render(
+		ui,
+		append([]immgo.RenderOption{
+			immgo.WithKind("sl-dropdown"),
+			immgo.WithOpen(),
+			immgo.WithEventHandler("sl-select", func(evt interface{}) {
+				*choice = evt.(map[string]interface{})["detail"].(map[string]interface{})["itemValue"].(string)
+			}),
+		}, opts...)...
+	)
+
+	Button(ui, WithSlot("trigger"), immgo.WithAttribute("textContent", "Dropdown"))
+	immgo.Render(ui, immgo.WithKind("sl-menu"), immgo.WithOpen())
+	for _, choice := range choices {
+		immgo.Render(
+			ui, 
+			immgo.WithKind("sl-menu-item"), 
+			immgo.WithKey(choice), 
+			immgo.WithAttribute("textContent", choice),
+			immgo.WithAttribute("value", choice),
+		)
+	}
+	immgo.Close(ui)
+
+	immgo.Close(ui)
+
+	return *choice
+}
+
+func Button(ui *immgo.Renderer, opts ...immgo.RenderOption) bool {
+	state := immgo.State(ui, false)
+
+	allOpts := append(
+		[]immgo.RenderOption{
+			immgo.WithKind("sl-button"),
+			immgo.WithEventHandlers(immgo.EventHandlers {
+				"click": func(_event interface{}) {
+					*state = true
+				},
+			}),
+		},
+		opts...
+	)
+
+	immgo.Render(
+		ui,
+		allOpts...
+	)
+
+	r := *state
+	*state = false
+	return r
+}
+
+func ShoelaceAssets(ui *immgo.Renderer) bool {
+	cssLoaded := immgo_web.Link(ui, "stylesheet", "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.73/dist/themes/light.css")
+	scriptLoaded := immgo_web.Script(ui, "module","https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.73/dist/shoelace.js")
+
+	return cssLoaded && scriptLoaded
+}
