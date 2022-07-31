208f4ff72d1020a517dabc0429d4a2562d08c6f8
Leigh Stewart <leigh.stewart@anchorlabs.com>
2022-07-31 19:24:19 +0000

    add last ex

--- go/web/examples/all_apps/main.go
+++ go/web/examples/all_apps/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	immgo_web "github.com/snapper-labs/immediate/go/web"
 	sevenGui "github.com/snapper-labs/immediate/go/web/examples/7gui/app"
+	bugReporter "github.com/snapper-labs/immediate/go/web/examples/bug_reporter/app"
 	deployLauncher "github.com/snapper-labs/immediate/go/web/examples/deploy_launcher/app"
 )
 
@@ -13,5 +14,6 @@ func main() {
 	}
 	immgo_web.Handle("deploy_launcher", deployLauncherApp)
 	immgo_web.Handle("7gui", &sevenGui.App{})
+	immgo_web.Handle("bug_reporter", &bugReporter.App{})
 	immgo_web.Serve("localhost:8081")
 }
