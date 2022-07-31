9ca90e8d2f9eec93b72e786e61ddabf89b3d6c73
Leigh Stewart <leigh.stewart@anchorlabs.com>
2022-07-31 19:56:18 +0000

    fix

--- go/web/main.js
+++ go/web/main.js
@@ -255,7 +255,6 @@
     } else {
       endpoint = "ws:";
     }
-    console.log("foo " + loc.pathname);
     endpoint += "//" + loc.host;
     endpoint += loc.pathname.substring(0, loc.pathname.length - 1);
     endpoint += "/ws";
