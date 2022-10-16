cab40083f7f2fe4b06da43db871777b7445e3424
Leigh Stewart <leighst@users.noreply.github.com>
2022-10-16 15:55:02 +0000

    storybook

---
+++ toolkit/src/stories/Button.ts
@@ -0,0 +1,40 @@
+import { html } from 'lit-html';
+import { styleMap } from 'lit-html/directives/style-map.js';
+import './button.css';
+
+export interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({ primary, label, onClick }: ButtonProps) => {
+  
+
+  return html`
+    <button
+      
+    >
+      ${label}
+    </button>
+  `;
+};
