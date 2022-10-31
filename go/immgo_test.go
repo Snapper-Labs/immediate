2804b3930c7edbbdedbcda1af78e608bef659a4e
Ankit Kumar <ankit@apkumar.com>
2022-10-31 13:59:13 -0400

    state tests

--- go/immgo_test.go
+++ go/immgo_test.go
@@ -2,6 +2,7 @@ package immgo
 
 import (
 	"testing"
+	"time"
 )
 
 func check(err error) {
@@ -77,5 +78,75 @@ func TestRendererSimple(t *testing.T) {
 	childrenAfter = hostRootRef.Children()
 	if len(childrenAfter) != 0 {
 		t.Errorf("Expected nodes to be removed; %d", len(childrenAfter))
+	}
+}
+
+func TestStateSimple(t *testing.T) {
+	renderFunc := func(parent *RenderNode) {
+		count, setCount := State(parent, 0)
+
+		// This should trigger a re-render.
+		//
+		// Note that state changes show up _after_ the end of the render.
+		setCount(*count + 1)
+		Render(parent, ElementDescription { Kind: "test", Properties: Properties { Attributes: Attributes { "count": *count } } })
+	}
+
+	hostTree := NewInmemHostTree()
+	drv := NewDriver(hostTree, hostTree.GetRoot(), renderFunc)
+
+	check(drv.Update(false))
+
+	// check the tree
+	hostRootRef := hostTree.GetNodeRef(hostTree.GetRoot())
+	if len(hostRootRef.Children()) != 2 {
+		t.Errorf("Expected 1 child, got %d", len(hostRootRef.Children()))
+	}
+
+	if hostRootRef.Children()[1].data.properties.Attributes["count"].(int) != 0 {
+		t.Errorf("Expected count to be 1, got %d", hostRootRef.Children()[1].data.properties.Attributes["count"].(int))
+	}
+
+	check(drv.Update(false))
+	check(drv.Update(false))
+
+	if hostRootRef.Children()[1].data.properties.Attributes["count"].(int) != 2 {
+		t.Errorf("Expected count to be 1, got %d", hostRootRef.Children()[1].data.properties.Attributes["count"].(int))
+	}
+}
+
+func TestStateAsync(t *testing.T) {
+	renderFunc := func(parent *RenderNode) {
+		count, setCount := State(parent, 0)
+
+		go func() {
+			time.Sleep(100 * time.Millisecond)
+			setCount(*count + 1)
+		}()
+
+		Render(parent, ElementDescription { Kind: "test", Properties: Properties { Attributes: Attributes { "count": *count } } })
+	}
+
+	hostTree := NewInmemHostTree()
+	drv := NewDriver(hostTree, hostTree.GetRoot(), renderFunc)
+
+	check(drv.Update(false))
+
+	// check the tree
+	hostRootRef := hostTree.GetNodeRef(hostTree.GetRoot())
+	if len(hostRootRef.Children()) != 2 {
+		t.Errorf("Expected 1 child, got %d", len(hostRootRef.Children()))
+	}
+
+	if hostRootRef.Children()[1].data.properties.Attributes["count"].(int) != 0 {
+		t.Errorf("Expected count to be 1, got %d", hostRootRef.Children()[1].data.properties.Attributes["count"].(int))
+	}
+
+	// wait 200 ms so we know the async set has been called.
+	time.Sleep(200 * time.Millisecond)
+
+	check(drv.Update(false))
+	if hostRootRef.Children()[1].data.properties.Attributes["count"].(int) != 1 {
+		t.Errorf("Expected count to be 2, got %d", hostRootRef.Children()[1].data.properties.Attributes["count"].(int))
 	}
 }
